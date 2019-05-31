5c0817288a8398c68d346c6292bd12fba5d190a9
Phillip M. Bitzer <pm.bitzer@uah.edu>
2019-05-31 16:09:07 -0500

    Add a cross correlation function

--- utilities/arrays.py
+++ utilities/arrays.py
@@ -105,3 +105,152 @@ def rebin(arr, shape, scheme=None):
         
     return new_arr
    
+
+def xcorr(data1, data2, times1=None, times2=None):
+    """
+    Given two arrays, find the lag that maximizes the cross correlation.
+    
+    For two data arrays, usually time series, this function will find the 
+    lag between the arrays that gives the maximum of the cross correlation.
+    
+    Before cross correlating, the mean of each data is subtracted from
+    the data.
+    
+    If corresponding time arrays for the data is not given, the lag is
+    reported as the number of elements.
+    
+    If the corresponding time arrays are given, then these times are 
+    interpolated to a common time grid. The lag is then reported in units
+    of time. 
+    
+    These time arrays will be shifted by the minimum of `times1`, `times2`.
+    The smaller of these minimums will be subtracted from each of the times
+    arrays. This has no effect on the lag, since we're only interested in the
+    difference between the two data sets. It does help the case if we
+    have large numbers (like in the case you pass in a `numpy.datetime64[ns]`
+    set of times).
+
+    Generally speaking, these arrays do not have to be "times", 
+    just some measure of how the data is spaced out. Right now, it only has
+    only really been tested on times, however.
+
+    Note: interpolation uses SciPy's UnivariateSpline, not interp1d
+    See: `<https://github.com/scipy/scipy/issues/4304>`_
+    
+    Code based on info found here:
+    `<https://mail.scipy.org/pipermail/scipy-user/2011-December/031177.html>`_
+
+    Note: Future implementation may use pycorrelate. See
+    `<https://pycorrelate.readthedocs.io/en/latest/index.html>`_
+
+    Parameters
+    -----------
+    data1 : array
+        The reference data.
+    data2 : array
+        The data to be correlated against.
+    times1 : array
+        The times of each element of `data1`. If given as a `np.datetime64`, 
+        this will be converted to a 64 bit integer. In this case, the lag
+        is units of the `np.datetime64` you use. For example, if
+        you provide `np.datetime64[ns]`, then the return value for the lags 
+        is in units of nanoseconds. 
+    times2 : array
+        The times of each element of `data2`. If given as a `np.datetime64`, 
+        this will be converted to a 64 bit integer. See `times1` for 
+        additional notes. 
+    
+    Returns
+    -------
+    tuple : 
+        - The lag of max correlation. A positive lag means the second array
+        is after the first.
+        - The lags; this will be the index of the lags. The size of the array
+          is `data1.size + data2.size-1`
+        - The cross correlation: the value of the cross correlation for the lags
+
+    Examples
+    ---------
+
+    import numpy as np
+
+    # make a triangle wave
+    x1 = np.array([0, 0, 0, 0, 0, 0])
+    x2 = np.array(np.arange(10))+1
+    _x3 = np.array(np.arange(10))
+    x3 = _x3[::-1] # reverse the array
+
+    x = np.concatenate((x2, x3, x3))
+
+    forced_lag = 4
+    y = np.roll(x, forced_lag)  # make another array that lags the orig
+
+    print(xcorr(x, y))  # should get value of forced_lag
+    """
+
+    import scipy.signal as sig
+    from scipy.interpolate import UnivariateSpline
+    
+    if (times1 is None) or (times2 is None):
+        # No times? Just find the lag
+        _data1 = data1
+        _data2 = data2
+
+        dist_per_lag = 1
+    else:
+        # Since we have times for each array, we will interpolate the data
+        # to a common set of times. 
+        # TODO check to see if both times1, times2 are given        
+        
+        # The code for datetime is 'M'...
+        if times1.dtype.kind == 'M': 
+            times1 = times1.astype('int64')
+        if times2.dtype.kind == 'M':
+            times2 = times2.astype('int64')
+        
+        # We're only interested in delta between the two data arrays,
+        # so we are going to shift the times:
+        # TODO use a minmax function
+        min_t = np.min(np.concatenate((times1, times2)))
+        max_t = np.max(np.concatenate((times1, times2)))
+
+        times1 = times1 - min_t
+        times2 = times2 - min_t
+        
+        # Define the interpolations
+        interp_args = {'ext':0, 'k':5, 's':0}  # keywords for UnivariateSpline
+        
+        intp1 = UnivariateSpline(times1, data1, **interp_args)
+        intp2 = UnivariateSpline(times2, data2, **interp_args)
+        
+        # Now, we'll go about building the common time grid
+        dt1 = np.min(np.diff(times1))
+        dt2 = np.min(np.diff(times2))
+        dist_per_lag = np.min((dt1, dt2))
+        dist_per_lag/= 10  # We are going to create a times array on a finer grid
+                
+        times_common = np.arange(0, max_t-min_t, dist_per_lag)
+        
+        # Now, interpolate the data to this grid
+        _data1 = intp1(times_common)
+        _data2 = intp2(times_common)
+    
+    # Before correlation, we should subtract off means:
+    # Note: avoiding compound ops to avoid any mutablity issues
+    _data1 = _data1 - np.mean(_data1)
+    _data2 = _data2 - np.mean(_data2)
+    
+    # Do the cross correlation, sweeping the data over each other.
+    # This will return data1.size+data2.size-1 elements
+    cross_corr = sig.correlate(_data1, _data2, "full")
+    
+    max_ind = np.argmax(cross_corr)
+    
+    # Now, generate an "axis" of lags
+    lags = np.arange(cross_corr.size) - (_data1.size-1)
+    
+    # Get the lags for the max of the correlation. 
+    loc = -lags[max_ind] * dist_per_lag
+        
+    return loc, lags, cross_corr
+    
